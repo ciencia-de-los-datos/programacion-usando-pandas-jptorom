daebb98675b8dca2ab2d71f4276d88db9152c9e0
jptorom <101909873+jptorom@users.noreply.github.com>
2022-04-17 16:29:16 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -75,7 +75,15 @@ def pregunta_04():
     E    4.785714
     Name: _c2, dtype: float64
     """
-    return
+    import pandas as pd
+
+    tbl0 = pd.read_csv("tbl0.tsv", sep="\t")
+    tbl1 = pd.read_csv("tbl1.tsv", sep="\t")
+    tbl2 = pd.read_csv("tbl2.tsv", sep="\t")
+    '''Cuenta las veces que aparece cada letra y los ordena'''
+    Contador = tbl0.groupby('_c1')._c2.mean()
+    
+    return Contador
 
 
 def pregunta_05():
